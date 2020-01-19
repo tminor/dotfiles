660d32c61d0f60c77f325ae512d000ffdd683dbc
tminor <thomas.minor@usg.edu>
2020-01-18 21:44:05 -0500

    emacs: Disable word count in Doom modeline

--- emacs/.emacs.d/lisp/init-modeline.el
+++ emacs/.emacs.d/lisp/init-modeline.el
@@ -11,10 +11,10 @@
   (require 'all-the-icons)
   :config
   (setq doom-modeline-buffer-file-name-style 'truncate-all)
-  (setq doom-modeline-enable-word-count t)
   (setq doom-modeline-icon t)
   (setq doom-modeline-height 45)
-  :hook (after-init . doom-modeline-init))
+  :hook
+  (after-init . doom-modeline-init))
 
 (provide 'init-modeline)
 ;;; init-modeline.el ends here
