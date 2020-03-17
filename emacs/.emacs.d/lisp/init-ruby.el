b0c919892f196037ed352c15d59ee94321dc8685
tminor <thomas.minor@usg.edu>
2020-03-17 11:44:36 -0400

    ruby: Add improvements
    Adds and fixes some problems:
    - Properly fold if/unless blocks
    - Remove robe mode in favor of lsp-mode
    - Add ruby-refactor

--- emacs/.emacs.d/lisp/init-ruby.el
+++ emacs/.emacs.d/lisp/init-ruby.el
@@ -15,7 +15,7 @@
     '(add-to-list
       'hs-special-modes-alist
       `(ruby-mode
-        ,(rx (or "def" "class" "module" "do" "{" "[" "(")) ; Block start
+        ,(rx (or "def" "class" "module" "do" "{" "[" "(" "if" "unless" "case")) ; Block start
         ,(rx (or "}" "]" ")" "end"))		           ; Block end
         ,(rx (or "#" "=begin"))		; Comment start
         ruby-forward-sexp nil)))
@@ -30,17 +30,17 @@
   (exec-path-from-shell-initialize)
   :hook
   (ruby-mode . eldoc-mode)
-  (ruby-mode . yard-mode)
-  (ruby-mode . robe-mode))
-
-(use-package robe
-  :hook
-  (ruby-mode . robe-mode)
-  :config
-  (eval-after-load 'company
-    '(push 'company-robe company-backends)))
+  (ruby-mode . yard-mode))
 
 (use-package yard-mode)
+
+(use-package ruby-refactor
+  :general
+  (:keymaps 'ruby-mode-map
+   :states '(normal motion)
+   "gr" 'emr-show-refactor-menu)
+  :hook
+  (ruby-mode . (lambda () (ruby-refactor-mode-launch))))
 
 (provide 'init-ruby)
 ;;; init-ruby.el ends here
