5f440a1b378e28499eff4cd0db644e8a3bafd6a9
tminor <thomas.minor@usg.edu>
2020-05-15 10:48:39 -0400

    emacs modeline: Add org-pomodoro segment and minimal exwm modeline

--- emacs/.emacs.d/lisp/init-modeline.el
+++ emacs/.emacs.d/lisp/init-modeline.el
@@ -7,14 +7,60 @@
 ;;; Code:
 
 (use-package doom-modeline
+  :straight
+  (:host github :repo "seagle0128/doom-modeline")
   :init
   (require 'all-the-icons)
   :config
-  (setq doom-modeline-buffer-file-name-style 'truncate-all)
-  (setq doom-modeline-icon t)
-  (setq doom-modeline-height 45)
+  (setq doom-modeline-buffer-file-name-style 'truncate-all
+	doom-modeline-icon t
+	doom-modeline-height 45
+	all-the-icons-scale-factor 1
+	doom-modeline-buffer-encoding nil)
+  (doom-modeline-def-segment org-pomodoro
+    "Displays `org-pomodoro` timer."
+    (if (doom-modeline--active)
+	(let ((task (if org-clock-current-task
+	      		(truncate-string-to-width org-clock-current-task
+	      					  30)))
+	      (face (cl-case org-pomodoro-state
+		      (:pomodoro 'org-pomodoro-mode-line)
+		      (:overtime 'org-pomodoro-mode-line-overtime)
+		      (:short-break 'org-pomodoro-mode-line-break)
+		      (:long-break 'org-pomodoro-mode-line-break)))
+	      (s (cl-case org-pomodoro-state
+		   (:pomodoro
+		    (propertize org-pomodoro-format 'face 'org-pomodoro-mode-line))
+		   (:overtime
+		    (propertize org-pomodoro-overtime-format
+				'face 'org-pomodoro-mode-line-overtime))
+		   (:short-break
+		    (propertize org-pomodoro-short-break-format
+				'face 'org-pomodoro-mode-line-break))
+		   (:long-break
+		    (propertize org-pomodoro-long-break-format
+				'face 'org-pomodoro-mode-line-break)))))
+	  (when (and (org-pomodoro-active-p) (> (length s) 0))
+	    (format s (string-join `(,(propertize
+				       (org-pomodoro-format-seconds)
+				       'face face)
+				     ,task) " "))))))
+  (doom-modeline-def-modeline 'main
+    '(bar workspace-name window-number modals matches buffer-info remote-host
+	  parrot selection-info)
+    '(objed-state misc-info org-pomodoro persp-name battery grip irc mu4e gnus
+		  github debug lsp minor-modes input-method indent-info
+		  buffer-encoding major-mode process vcs checker))
+  (doom-modeline-def-modeline 'tm/doom-modeline-exwm
+    '(bar modals misc-info) '(org-pomodoro battery major-mode process))
+  (defun tm/set-exwm-modeline ()
+    "Set a minimal modeline if exwm-mode is enabled."
+    (if (eq major-mode 'exwm-mode)
+	(doom-modeline-set-modeline 'tm/doom-modeline-exwm)
+      (doom-modeline-set-modeline 'main)))
   :hook
-  (after-init . doom-modeline-init))
+  (after-init . doom-modeline-init)
+  (buffer-list-update . tm/set-exwm-modeline))
 
 (provide 'init-modeline)
 ;;; init-modeline.el ends here
