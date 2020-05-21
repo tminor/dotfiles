198b420a2edd108375cb51037f01974dadc6678b
tminor <thomas.minor@usg.edu>
2020-05-21 08:23:09 -0400

    emacs gc: Contextually set GC

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,16 +6,28 @@
 
 ;;; Code:
 
-;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
+;; Produce backtraces when errors occur: can be helpful to diagnose
+;; startup issues
 (setq debug-on-error nil)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
-            (init-gc-cons-threshold (* 128 1024 1024)))
-    (setq gc-cons-threshold init-gc-cons-threshold)
-      (add-hook 'emacs-startup-hook
-		            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+	    (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+;; http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
+(defun tm/minibuffer-increase-gc-cons-threshold ()
+  "Disable GC before entering the minibuffer."
+  (setq gc-cons-threshold most-positive-fixnum))
+(defun tm/miniguffer-decrease-gc-cons-threshold ()
+  "Enable GC when leaving the minibuffer."
+  (setq gc-cons-threshold (* 20 1024 1024)))
+
+(add-hook 'minibuffer-setup-hook #'tm/minibuffer-increase-gc-cons-threshold)
+(add-hook 'minibuffer-exit-hook #'tm/miniguffer-decrease-gc-cons-threshold)
 
 (require 'init-package)
 (require 'init-key-bindings)
