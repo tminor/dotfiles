63b2548f99e71ce6ae2333db7106aa8b734ce4e7
tminor <thomas.minor@usg.edu>
2020-05-21 08:18:31 -0400

    emacs dired: Remove unused packages/add dired-sidebar

--- emacs/.emacs.d/lisp/init-dired.el
+++ emacs/.emacs.d/lisp/init-dired.el
@@ -6,68 +6,26 @@
 ;;
 ;;; Code:
 
-;; (use-package dired+
-;;   :init
-;;   (with-eval-after-load "dired" '(require 'dired+)))
+;; (use-package dired-plus
+;;   :after 'dired)
 
-(use-package dired-collapse)
+(use-package all-the-icons-dired
+  :straight
+  (:host github :repo "jtbm37/all-the-icons-dired")
+  :hook
+  (dired-mode . all-the-icons-dired-mode))
 
-(use-package dired-subtree)
-
-(use-package diredfl)
-
-(use-package dired-rainbow
-  :after dired
-  :demand t
+(use-package dired-sidebar
+  :straight
+  (:host github :repo "jojojames/dired-sidebar")
+  :commands (dired-sidebar-toggle-sidebar)
+  :hook
+  (dired-sidebar-mode-hook . (lambda ()
+			       (unless (file-remote-p default-directory)
+				 (auto-revert-mode))))
   :config
-  (dired-rainbow-define-chmod executable-unix "#0F0" "-[rw-]+x.*")
-  (dired-rainbow-define
-   immediate
-   "#FEDA98"
-   "\\(?:\\(?:R\\(?:EADME\\|eadme\\)\\|readme\\)\\)\\(?:[^ ]*?\\)?\\|Makefile\\|Cargo\\.toml\\|SConstruct\\|CMakeLists\\.txt\\|build\\.gradle\\|Rakefile\\|Gruntfile\\.js\\|Gruntfile\\.coffee")
-  (dired-rainbow-define
-   image
-   "#3EDAD4"
-   ("png" "jpeg" "jpg" "gif" "bmp" "tiff" "tif"
-    "ppm" "pgm" "pbm" "pnm" "webp" "raw" "arw"
-    "svg" "stl" "eps" "dvi" "ps" "cbr"
-    "cbz" "xpm" "ico" "cr2" "orf" "nef"))
-  (dired-rainbow-define
-   video
-   "#742FD1"
-   ("avi" "flv" "m2v" "mkv" "mov" "mp4" "mpeg"
-    "mpg" "ogm" "ogv" "vob" "wmv" "webm" "m2ts"
-    "ts"))
-  (dired-rainbow-define
-   music
-   "#742FD1"
-   ("aac" "m4a" "mp3" "ogg" "wma" "mka" "opus"))
-  (dired-rainbow-define
-   lossless-music
-   "#A044A0"
-   ("alac" "ape" "flac" "wav"))
-  (dired-rainbow-define
-   crypto
-   "#DD0B53"
-   ("asc" "enc" "gpg" "pgp" "sig" "signature" "pfx" "p12"))
-  (dired-rainbow-define
-   document
-   "#9A99E7"
-   ("djvu" "doc" "docx" "dvi" "eml" "eps" "fotd"
-    "odp" "odt" "pdf" "ppt" "pptx" "rtf"
-    "xls" "xlsx"))
-  (dired-rainbow-define
-   compiled
-   "#FFB5D8"
-   ("class" "elc" "hi" "o" "pyc")))
-
-(use-package dired-du
-  :straight (:host github :repo "emacsmirror/dired-du")
-  :config
-  (setq dired-du-size-format t))
-
-(use-package dired-plus
-  :after 'dired)
+  (setq dired-sidebar-theme 'icons
+	dired-sidebar-use-term-integration t))
 
 (provide 'init-dired)
 ;;; init-dired.el ends here
