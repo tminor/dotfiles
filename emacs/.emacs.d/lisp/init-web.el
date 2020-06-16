2423f2676c05957b02ffb04d7de68ed91fcddd9c
tminor <thomas.minor@usg.edu>
2020-06-16 10:16:38 -0400

    emacs/web-mode: Add key bindings and config

--- emacs/.emacs.d/lisp/init-web.el
+++ emacs/.emacs.d/lisp/init-web.el
@@ -7,8 +7,12 @@
 ;;; Code:
 
 (use-package web-mode
+  :general
+  (:keymaps 'web-mode-map
+   :states '(normal motion)
+   "TAB" 'web-mode-fold-or-unfold)
   :config
-  (setq web-mode-engines-alist '(("jsp" . "\\.jsp"))))
+  (setq web-mode-engines-alist `(("jsp" . ,(rx ".jsp")))))
 
 (provide 'init-web)
 ;;; init-web.el ends here
