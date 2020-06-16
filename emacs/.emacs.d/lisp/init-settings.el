c28d21bea244a819ec1361cabd2a8debbfcadcb3
tminor <thomas.minor@usg.edu>
2020-06-16 10:15:09 -0400

    emacs/scroll: Don't jump around while scrolling

--- emacs/.emacs.d/lisp/init-settings.el
+++ emacs/.emacs.d/lisp/init-settings.el
@@ -90,5 +90,10 @@
 
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
+;; nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
 (provide 'init-settings)
 ;;; init-settings.el ends here
