37c8de9493e85af9fe54ade149e65ec41eca85cb
tminor <thomas.minor@usg.edu>
2020-06-16 09:53:31 -0400

    emacs/help: Add Info-mode packages

--- emacs/.emacs.d/lisp/init-help.el
+++ emacs/.emacs.d/lisp/init-help.el
@@ -23,7 +23,10 @@
     "" '(:which-key "help prefix" :ignore t)
     "s" 'helpful-symbol
     "k" 'helpful-key
-    "m" 'man))
+    "m" 'man
+    "Lf" 'list-faces-display)
+  :config
+  (setq find-function-C-source-directory "~/src/emacs-26.3"))
 
 (use-package info-plus
   :general
@@ -32,7 +35,28 @@
     :prefix-command 'tm/help-prefix-command
     :prefix-map 'tm/help-prefix-map
     "" '(:which-key "help prefix" :ignore t)
-    "i" 'info))
+    "i" 'info)
+  :hook
+  (Info-selection . (lambda () (unless (eq buffer-face-mode 'variable-pitch)
+			    (variable-pitch-mode))))
+  (Info-mode . (lambda () (unless (eq buffer-face-mode 'variable-pitch)
+		       (variable-pitch-mode)))))
+
+(use-package info-lookup
+  :straight nil
+  :general
+  (tm/leader-def
+    :infix "h"
+    :prefix-command 'tm/help-prefix-command
+    :prefix-map 'tm/help-prefix-map
+    "S" 'info-lookup-symbol))
+
+(use-package info-colors
+  :straight
+  (:host github :repo "ubolonton/info-colors")
+  :hook
+  (Info-mode . rainbow-delimiters-mode)
+  (Info-selection . info-colors-fontify-node))
 
 (provide 'init-help)
 ;;; init-help.el ends here
