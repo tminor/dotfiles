1765796b53ce261d87e931f64ef8f59fe24eee18
tminor <thomas.minor@usg.edu>
2020-08-05 11:18:07 -0400

    emacs: Add pkg/func for cycling var styles in prog-mode

--- emacs/.emacs.d/lisp/init-ide.el
+++ emacs/.emacs.d/lisp/init-ide.el
@@ -94,5 +94,28 @@
 
 (use-package emr)
 
+(use-package string-inflection
+  :preface
+  (defun tm/string-inflection-cycle-auto ()
+    "switching by major-mode"
+    (interactive)
+    (cond
+     ;; for emacs-lisp-mode
+     ((eq major-mode 'emacs-lisp-mode)
+      (string-inflection-all-cycle))
+     ;; for python
+     ((eq major-mode 'python-mode)
+      (string-inflection-python-style-cycle))
+     ;; for java
+     ((eq major-mode 'java-mode)
+      (string-inflection-java-style-cycle))
+     (t
+      ;; default
+      (string-inflection-all-cycle))))
+  :general
+  (:keymaps 'prog-mode-map
+   :states '(motion  normal iedit)
+   "U" 'tm/string-inflection-cycle-auto))
+
 (provide 'init-ide)
 ;;; init-ide.el ends here
