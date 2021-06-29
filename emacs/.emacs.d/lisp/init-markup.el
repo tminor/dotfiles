bfd79846725eac2c5a02dbb421c728842b0c8eda
Thomas Minor <tminor@terminus-est.sss.usg.edu>
2021-06-29 11:41:00 -0400

    emacs: Add markup mode configuration
    - Some `yaml-mode` improvements;
    - And add `mermaid-mode`.

--- emacs/.emacs.d/lisp/init-markup.el
+++ emacs/.emacs.d/lisp/init-markup.el
@@ -21,8 +21,21 @@
 (use-package yaml-mode
   :straight
   (:host github :repo "yoshiki/yaml-mode")
+  :general
+  (:keymaps 'yaml-mode-map
+   :states '(normal motion)
+   "TAB" 'outline-toggle-children)
+  :hook
+  (yaml-mode . tm/yaml-mode-outline-hook)
   :init
-  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+  (defun tm/yaml-outline-level ()
+    "Return the outline level based on the indentation, hardcoded at 2 spaces."
+    (s-count-matches "[ ]\\{2\\}" (match-string 0)))
+  (defun tm/yaml-mode-outline-hook ()
+    (outline-minor-mode)
+    (setq outline-regexp "^\\([ ]\\{2\\}\\)*\\([-] \\)?\\([\"][^\"]*[\"]\\|[a-zA-Z0-9_-]*\\): *\\([>|]\\|&[a-zA-Z0-9_-]*\\)?$")
+    (setq outline-level 'tm/yaml-outline-level)))
 
 (use-package csv-mode)
 
@@ -38,6 +51,10 @@
 (use-package flycheck-mmark
   :hook
   (flycheck-mode . (lambda () (flycheck-mmark-setup))))
+
+(use-package mermaid-mode
+  :config
+  (setq mermaid-mmdc-location "/home/tminor/node_modules/.bin/mmdc"))
 
 (provide 'init-markup)
 ;;; init-markup.el ends here
