4bf056a1231df3383dbfd0343b5833a66d461771
Thomas Minor <tminor@terminus-est.sss.usg.edu>
2021-06-29 11:51:57 -0400

    emacs: Add lsp-puppet

--- emacs/.emacs.d/lisp/init-puppet.el
+++ emacs/.emacs.d/lisp/init-puppet.el
@@ -11,5 +11,12 @@
   (setq flycheck-puppet-parser-executable (executable-find "puppet"))
   (setq flycheck-puppet-lint-executable (executable-find "puppet-lint")))
 
+(use-package lsp-puppet
+  :straight
+  (:host github :repo "tminor/lsp-puppet")
+  :init
+  (setq lsp-puppet-server-install-dir "/home/tminor/.emacs.d/.cache/lsp/puppet/puppet-editor-services/"
+        lsp-puppet-modulepath '("/home/tminor/src/puppet/modules" "~/src/puppet/sssmodules")))
+
 (provide 'init-puppet)
 ;;; init-puppet.el ends here
